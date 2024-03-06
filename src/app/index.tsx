de282ebe921add459dd51eb299e3157430a3cacd
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-03-06 19:38:50 -0300

    start style initial section

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,24 +1,64 @@
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { router } from 'expo-router';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Link href='/players/'>
-        <Text style={styles.title}>Jogar</Text>
-      </Link>
+      <View style={styles.body}>
+        <View>
+          <Image
+            source={require('../../assets/images/logo.webp')}
+            style={styles.image}
+          />
+        </View>
+        <TouchableOpacity
+          onPress={() => router.push('/players/')}
+          style={styles.start}
+        >
+          <Text style={styles.textStart}>Jogar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
   },
-  title: {
-    fontSize: 20,
+  body: {
+    height: '100%',
+    width: '100%',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  start: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderColor: '#9966cc',
+    shadowColor: '#9966cc',
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+    elevation: 24,
+
+    borderWidth: 1,
+    width: '80%',
+    height: 60,
+    borderRadius: 10,
+  },
+  textStart: {
+    fontSize: 30,
     fontWeight: 'bold',
+  },
+  image: {
+    width: 300,
+    height: 300,
   },
 });

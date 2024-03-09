21c7fc814446705288298637141b08348eb13d41
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-03-08 21:34:19 -0300

    add input with image

--- src/app/players/index.tsx
+++ src/app/players/index.tsx
@@ -1,5 +1,4 @@
 import { useStorePlayer } from '@/src/store/players';
-import { Link } from 'expo-router';
 import React, { useEffect } from 'react';
 import {
   StyleSheet,
@@ -7,7 +6,8 @@ import {
   TextInput,
   View,
   ScrollView,
-  Button,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
 
 export default function Players() {
@@ -20,44 +20,60 @@ export default function Players() {
   }, [players[players.length - 1].name]);
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.title}>Adicione os jogadores</Text>
       <ScrollView style={styles.playerContainer}>
         {players.map((player, index) => (
           <View key={index} style={styles.playerRow}>
-            <TextInput
-              style={styles.input}
-              onChangeText={(text) => updatePlayer(text, index)}
-              value={player.name}
-              placeholder={`Novo jogador`}
-            />
-            {players.length - 1 !== index && (
-              <Button
-                title='Remover'
-                onPress={() => removePlayer(index)}
-                color='#ff4444'
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                onChangeText={(text) => updatePlayer(text, index)}
+                value={player.name}
+                placeholder={`Novo jogador`}
               />
-            )}
+              {players.length - 1 !== index && (
+                <TouchableOpacity onPress={() => removePlayer(index)}>
+                  <Image
+                    style={styles.icon}
+                    source={require('../../../assets/icons/bin.png')}
+                  />
+                </TouchableOpacity>
+              )}
+            </View>
           </View>
         ))}
       </ScrollView>
-      <Link href='/getWord/' asChild>
+      {/* <Link href='/getWord/' asChild>
         <Button title='Começar partida' color='#ff4444' />
-      </Link>
-    </ScrollView>
+      </Link> */}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
     paddingVertical: 50,
   },
   playerContainer: {
     width: '100%',
-    height: 0,
+  },
+  inputContainer: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderWidth: 2,
+    borderColor: '#9966cc',
+    padding: 10,
+    borderRadius: 10,
+  },
+  icon: {
+    width: 20,
+    height: 20,
   },
   title: {
     fontSize: 20,
@@ -65,16 +81,16 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   input: {
-    flex: 1,
+    width: '90%',
     height: 40,
-    margin: 12,
-    borderWidth: 1,
-    borderColor: '#000',
-    padding: 10,
+    // margin: 12,
   },
   playerRow: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'center',
     width: '90%',
+    alignSelf: 'center',
+    marginVertical: 10,
   },
 });

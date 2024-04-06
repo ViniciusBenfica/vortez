4720162367c32525724a8d32c7cd68f67a14bd0a
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-04-06 17:02:50 -0300

    ✅ add test to getWord page

--- src/app/__tests__/pages/getWord.spec.tsx
+++ src/app/__tests__/pages/getWord.spec.tsx
@@ -1,33 +1,64 @@
 import GetWord from "@/app/pages/getWord";
 import { useStorePlayer } from "@/store/players";
 import { fireEvent, render } from "@testing-library/react-native";
+import { router } from "expo-router";
 import React from "react";
 
-// jest.mock("expo-router", () => ({
-// 	router: {
-// 		push: jest.fn(),
-// 	},
-// }));
+let mockRandomPlayer = "Jogador 3";
+let mockPlayers = [{ name: "Jogador 1" }, { name: "Jogador 2" }];
+
+jest.mock("expo-router", () => ({
+	router: {
+		push: jest.fn(),
+	},
+}));
+
+jest.mock("@/questions/foods", () => ({
+	foodsName: ["banana"],
+}));
 
 jest.mock("@/store/players", () => ({
 	useStorePlayer: () => ({
 		getRandomPlayer: jest.fn(),
-		randomPlayer: "Jogador 1",
-		players: [{ name: "Jogador 1" }, { name: "Jogador 2" }],
+		randomPlayer: mockRandomPlayer,
+		players: mockPlayers,
 	}),
 }));
 
 describe("GetWord", () => {
+	beforeEach(() => {
+		mockRandomPlayer = "Jogador 3";
+		mockPlayers = [{ name: "Jogador 1" }, { name: "Jogador 2" }];
+	});
+
 	it("renders correctly", () => {
 		const { getByText } = render(<GetWord />);
 		expect(getByText(`Voce é o ${useStorePlayer().players[0].name}?`)).toBeTruthy();
-		fireEvent.press(getByText("Sim, sou eu!"));
+		expect(getByText("Sim, sou eu!")).toBeTruthy();
 	});
 
-	it("renders correctly when have interaction", async () => {
-		const { getByText, queryByText } = render(<GetWord />);
+	it("show word when player is not random", async () => {
+		const { getByText } = render(<GetWord />);
 		expect(getByText(`Voce é o ${useStorePlayer().players[0].name}?`)).toBeTruthy();
 		fireEvent.press(getByText("Sim, sou eu!"));
-		expect(queryByText("A palavra é")).toBeTruthy();
+		expect(getByText("A palavra é")).toBeTruthy();
+		expect(getByText("banana")).toBeTruthy();
+	});
+
+	it("show word when player is random", async () => {
+		mockRandomPlayer = "Jogador 1";
+		const { getByText } = render(<GetWord />);
+		expect(getByText(`Voce é o ${useStorePlayer().players[0].name}?`)).toBeTruthy();
+		fireEvent.press(getByText("Sim, sou eu!"));
+		expect(getByText("A palavra é")).toBeTruthy();
+		expect(getByText("Não sabe")).toBeTruthy();
+	});
+
+	it("navigates on button press", () => {
+		const { getByText } = render(<GetWord />);
+		expect(getByText(`Voce é o ${useStorePlayer().players[0].name}?`)).toBeTruthy();
+		fireEvent.press(getByText("Sim, sou eu!"));
+		fireEvent.press(getByText("Continuar"));
+		expect(router.push).toHaveBeenCalledWith("/pages/questions");
 	});
 });

b1a191aec03377359b4157129c4a4027351aa685
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-04-07 16:19:44 -0300

    ✅ add tests in result page

---
+++ src/app/__tests__/pages/result.spec.tsx
@@ -0,0 +1,47 @@
+import Result from "@/app/pages/result";
+import { useStorePlayer } from "@/store/players";
+import { fireEvent, render } from "@testing-library/react-native";
+import { router } from "expo-router";
+import React from "react";
+
+jest.mock("expo-router", () => ({
+	router: {
+		push: jest.fn(),
+	},
+}));
+
+jest.mock("@/store/players", () => ({
+	useStorePlayer: () => ({
+		players: [
+			{ name: "Jogador 1", votes: 4 },
+			{ name: "Jogador 2", votes: 2 },
+			{ name: "Jogador 3", votes: 1 },
+		],
+		randomPlayer: "Jogador 3",
+		clearVotes: jest.fn(),
+	}),
+}));
+
+describe("Result", () => {
+	it("renders correctly", () => {
+		const { getByText } = render(<Result />);
+		expect(getByText("O mais votado foi")).toBeTruthy();
+		expect(getByText("Quem não sabia a palavra")).toBeTruthy();
+	});
+
+	it("shows who got the most votes", () => {
+		const { getByDisplayValue } = render(<Result />);
+		expect(getByDisplayValue("Jogador 1")).toBeTruthy();
+	});
+
+	it("shows who doesn't know the word", () => {
+		const { getByDisplayValue } = render(<Result />);
+		expect(getByDisplayValue("Jogador 3")).toBeTruthy();
+	});
+
+	it("navigates on button press", () => {
+		const { getByText } = render(<Result />);
+		fireEvent.press(getByText("Jogar novamente"));
+		expect(router.push).toHaveBeenCalledWith("/");
+	});
+});

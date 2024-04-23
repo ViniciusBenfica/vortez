c965c3a64f464b6967bd46608cfac65c19dcde0b
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-04-23 11:09:29 -0300

    ✨ add themes

---
+++ src/questions/index.ts
@@ -0,0 +1,34 @@
+import { foodsName, foodsQuestions } from "./foods";
+
+export const theme = [
+	{
+		title: 'Comida',
+		theme: foodsName,
+		questions: foodsQuestions,
+	},
+	{
+		title: 'Animal',
+		theme: foodsName,
+		questions: foodsQuestions,
+	},
+	{
+		title: 'Esportes',
+		theme: foodsName,
+		questions: foodsQuestions,
+	},
+	{
+		title: 'Jogos',
+		theme: foodsName,
+		questions: foodsQuestions,
+	},
+	{
+		title: 'Música',
+		theme: foodsName,
+		questions: foodsQuestions,
+	},
+	{
+		title: 'Televisão',
+		theme: foodsName,
+		questions: foodsQuestions,
+	},
+]
